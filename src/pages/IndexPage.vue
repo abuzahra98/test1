4d3176693708cbc6a02ba7d588e86e49d7e3a626
“dawood-abuzahra” <dabuzahra@pando-soft.com>
2023-09-30 10:43:39 +0300

    test

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,340 @@
+<template>
+  <q-page>
+    <div class="firsSection">
+      <div class="row justify-center">
+        <div
+          class="q-mb-xl row justify-between col-xl-9 col-lg-9 col-md-10 col-sm-11 col-xs-11 q-pb-md items-center"
+          style="padding-block-start: 121px; border-block-end: 1px solid white"
+        >
+          <div
+            class="row col-xl-2 col-lg-2 col-md-3 col-sm-4 col-xs-12 justify-between items-center"
+          >
+            <div
+              class="header-text row col-xl-8 col-lg-8 col-md-8 col-sm-7 col-xs-4 q-my-sm"
+            >
+              تواصل معنا
+            </div>
+            <div
+              class="header-text row col-xl-4 col-lg-4 col-md-4 col-sm-1 col-xs-1 q-my-sm"
+            >
+              <img src="public/icons/search.svg" alt="" />
+            </div>
+            <div
+              class="header-text row col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4 q-my-sm justify-end hide-descktop"
+            >
+              <img src="public/icons/logo.svg" alt="" />
+            </div>
+          </div>
+
+          <div
+            class="row justify-between col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12 items-center"
+          >
+            <div class="header-text">رحله العميل</div>
+            <div class="header-text">تقنياتنا</div>
+            <div class="header-text">خدماتنا</div>
+            <div class="header-text">من نحن</div>
+            <div class="hide-image">
+              <img src="public/icons/logo.svg" alt="" />
+            </div>
+          </div>
+        </div>
+        <div
+          class="row col-xl-9 col-lg-9 col-md-10 col-sm-11 col-xs-11 justify-between q-mt-xl q-mb-xl custom"
+        >
+          <img
+            class="col-xl-4 col-lg-4 col-md-0 col-sm-0 col-xs-0 hide-mobile"
+            src="public/icons/aa.svg"
+            alt=""
+          />
+
+          <div class="text-right col-xl-7 col-lg-7 col-md-12 col-sm-12">
+            <div class="header-main-text">نقدم لكم خدمات بترولية</div>
+            <div class="header-title-text">تليق بكم</div>
+            <div
+              class="desc row items-start justify-end q-py-md"
+              style="text-align: right"
+            >
+              <div
+                class="col-xl-8 col-lg-8 col-md-10 col-sm-12 col-xs-12 q-mt-lg row"
+              >
+                سايد هي علامة تجارية جديدة و متطورة في مجال بيع الوقود، بالتجزئة
+                في المملكة العربية السعودية
+              </div>
+            </div>
+            <div class="row justify-end q-mt-xl">
+              <button class="header__btn">أستكشف</button>
+            </div>
+          </div>
+        </div>
+        <div class="row col-9 hide-mobile">
+          <div class="row col-4 justify-center q-mt-xl">
+            <div><img src="public/icons/click.svg" alt="" /></div>
+          </div>
+          <div class="row col-8 justify-between q-mt-xl">
+            <div style="margin-inline-start: 11rem">
+              <img src="public/icons/move.svg" alt="" />
+            </div>
+            <div><img src="public/icons/social.svg" alt="" /></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="secound__section row justify-center"
+      style="margin-block-start: 10rem"
+    >
+      <div
+        class="ss q-mb-xl row justify-center col-xl-9 col-lg-9 col-md-10 col-sm-11 col-xs-11 q-pb-md items-center any"
+      >
+        <div class="row col-xl-6 col-lg-6 col-md-6 col-sm-11">
+          <img style="width: 100%" src="public/icons/section-tow.svg" alt="" />
+        </div>
+        <div class="row col-xl-6 col-lg-6 col-md-6 col-sm-11 justify-end">
+          <div
+            class="col-4 row justify-center"
+            style="
+              padding-block: 15px;
+              font-size: 64px;
+              font-weight: 600;
+              border-bottom: 4px solid #743c7f;
+            "
+          >
+            قيمنا
+          </div>
+          <div class="row col-12 justify-end q-mt-lg">
+            <div
+              class="col-xl-10 col-lg-9 col-md-10 col-sm-11 q-px-sm q-mb-lg"
+              style="
+                color: var(--1-f-1-f-1-f, #1f1f1f);
+                text-align: right;
+                font-family: Changa;
+                font-size: 24px;
+                font-style: normal;
+                font-weight: 500;
+                line-height: normal;
+              "
+            >
+              قيمنا الأساسية هي البساطة والثقة والالتزام و يعمل الجميع في سايد
+              وفقًا لهذه القيم والإمتثال لجميع القوانين واللوائح ذات الصلة
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="therd__section row justify-center"
+      style="margin-block-start: 10rem"
+    >
+      <div
+        class="q-mb-xl row justify-center col-xl-10 col-lg-10 col-md-11 col-sm-11 col-xs-11 q-pb-md items-center"
+      >
+        <div
+          class="col-2 q-mb-xl row justify-center"
+          style="
+            padding-block: 15px;
+            font-size: 64px;
+            font-weight: 600;
+            border-bottom: 4px solid #743c7f;
+          "
+        >
+          هويتنا
+        </div>
+
+        <div class="row col-12 justify-center q-mb-xl custom__margin">
+          <div>
+            <img
+              class="col-xl-3 col-lg-3 col-md-6 q-mx-md no-margin-custom"
+              src="public/icons/avatar.svg"
+              alt=""
+            />
+            <div class="text-center three__text">المسجد</div>
+          </div>
+          <div>
+            <img
+              class="col-xl-3 col-lg-3 col-md-6 q-mx-md no-margin-custom"
+              src="public/icons/super.svg"
+              alt=""
+            />
+            <div class="text-center three__text">السوبر ماركت</div>
+          </div>
+          <div>
+            <img
+              class="col-xl-3 col-lg-3 col-md-6 q-mx-md no-margin-custom"
+              src="public/icons/any.svg"
+              alt=""
+            />
+            <div class="text-center three__text">المحطة</div>
+          </div>
+        </div>
+        <div class="row col-12 justify-center q-mt-xl margin__custom-button">
+          <div>
+            <img
+              class="col-xl-3 col-lg-3 col-md-6 q-mx-md no-margin-custom"
+              src="public/icons/avatar-2.svg"
+              alt=""
+            />
+            <div class="text-center three__text">المسجد من الداخل</div>
+          </div>
+          <div>
+            <img
+              class="col-xl-3 col-lg-3 col-md-6 q-mx-md no-margin-custom"
+              src="public/icons/super-2.svg"
+              alt=""
+            />
+            <div class="text-center three__text">مبنى الخدمات الفندقية</div>
+          </div>
+          <div>
+            <img
+              class="col-xl-3 col-lg-3 col-md-6 q-mx-md no-margin-custom"
+              src="public/icons/any-2.svg"
+              alt=""
+            />
+            <div class="text-center three__text">
+              مبنى الصيانة و غسيل السيارات
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <button class="last__btn">شاهد المزيد</button>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
+<style>
+.firsSection {
+  background-image: url('public/icons/first.svg');
+  width: 100%;
+  height: 900px;
+  background-size: cover;
+}
+
+.header-text {
+  color: #fff;
+  font-family: Changa;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 24px;
+  cursor: pointer;
+}
+
+.header-main-text {
+  color: #fff;
+  text-align: right;
+  font-family: Changa;
+  font-size: 74px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 104.5%; /* 66.88px */
+}
+
+.header-title-text {
+  color: #ddbddb;
+  font-family: Changa;
+  font-size: 64px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 104.5%;
+}
+
+.desc {
+  color: #fff;
+  text-align: right;
+  font-family: Changa;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+}
+
+.header__btn {
+  border-radius: 4px;
+  background: var(--743-c-7-f, #743c7f);
+  display: flex;
+  width: 133px;
+  padding: 10px 12px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border: none;
+  color: white;
+  cursor: pointer;
+}
+
+.three__text {
+  color: #303030;
+  text-align: center;
+  font-family: Changa;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin-block: 20px;
+}
+
+.last__btn {
+  border-radius: 15px;
+  background: #743c7f;
+  box-shadow: 0px 10px 20px 0px rgba(172, 87, 188, 0.3);
+  color: #fff;
+
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 47px; /* 335.714% */
+  padding: 4.5px 58px;
+  justify-content: center;
+  border: none;
+  margin-top: 5rem;
+  cursor: pointer;
+}
+
+@media screen and (max-width: 1030px) {
+  .any {
+    flex-direction: column-reverse !important;
+  }
+}
+@media screen and (max-width: 900px) {
+  .header-main-text {
+    font-size: 60px;
+  }
+
+  .custom__margin {
+    margin-block-end: 15px;
+  }
+  .margin__custom-button {
+    margin-block-start: 0;
+  }
+}
+@media screen and (max-width: 1100px) {
+  .hide-mobile {
+    display: none;
+  }
+}
+@media screen and (max-width: 500px) {
+  .header-main-text {
+    font-size: 55px;
+  }
+}
+@media screen and (max-width: 400px) {
+  .no-margin-custom {
+    margin-inline: 0 !important;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .hide-image {
+    display: none;
+  }
+}
+@media screen and (min-width: 600px) {
+  .hide-descktop {
+    display: none;
+  }
+}
+</style>

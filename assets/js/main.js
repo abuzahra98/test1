2063f6acaf8739067fb6d9224ac0429316683a42
“dawood-abuzahra” <dabuzahra@pando-soft.com>
2023-10-06 16:01:19 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,17 +28,24 @@ up.addEventListener('click',()=>{
 document.addEventListener("DOMContentLoaded", function () {
     var header = document.querySelector("header");
     var newContent = document.querySelector(".new-content");
+    var image = document.querySelector(".content-about_img");
   
     window.addEventListener("scroll", function () {
       // Get the height of the header
       var headerHeight = header.offsetHeight;
       var headerwedth = header.offsetWidth;
-      console.log('====================================');
-      console.log(headerwedth,'headerwedth');
-      console.log('====================================');
+
       // Get the current scroll position
       var scrollPosition = window.scrollY;
       var width = 900
+      if (headerwedth < 800) {
+        console.log('====================================');
+        console.log(headerwedth,'headerwedth');
+        console.log('====================================');
+        image.style.display = "none";
+        // image.classList.add("onTop");
+
+      }
       // Show the new-content div when scrolled past the header
       if (scrollPosition > headerHeight&& headerwedth > width) {
         newContent.style.display = "flex";

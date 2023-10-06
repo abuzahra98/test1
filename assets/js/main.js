6474f81c6e27266ca13b8ffa6a1dafedd9cfdda5
“dawood-abuzahra” <dabuzahra@pando-soft.com>
2023-10-06 15:42:29 +0300

    test

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+const menuBar = document.querySelector('.menu-bar');
+const sideBar = document.querySelector('.sidebar');
+const overlay = document.querySelector('.overlay');
+const lis = document.querySelectorAll('.list li');
+const up = document.querySelector('.up');
+
+console.log(lis);
+
+menuBar.addEventListener('click',() =>{
+    sideBar.classList.add('open')
+    overlay.classList.add('open')
+})
+
+overlay.addEventListener('click',()=>{
+    sideBar.classList.remove('open')
+    overlay.classList.remove('open')
+})
+
+lis.forEach(li =>{
+    li.addEventListener('click',() =>{
+        sideBar.classList.remove('open')
+        overlay.classList.remove('open')
+    })
+})
+up.addEventListener('click',()=>{
+    window.scroll(0,0)
+})
+document.addEventListener("DOMContentLoaded", function () {
+    var header = document.querySelector("header");
+    var newContent = document.querySelector(".new-content");
+  
+    window.addEventListener("scroll", function () {
+      // Get the height of the header
+      var headerHeight = header.offsetHeight;
+      var headerwedth = header.offsetWidth;
+      console.log('====================================');
+      console.log(headerwedth,'headerwedth');
+      console.log('====================================');
+      // Get the current scroll position
+      var scrollPosition = window.scrollY;
+      var width = 900
+      // Show the new-content div when scrolled past the header
+      if (scrollPosition > headerHeight&& headerwedth > width) {
+        newContent.style.display = "flex";
+        newContent.classList.add("onTop");
+        
+    } else {
+        newContent.style.display = "none";
+      }
+    });
+  });

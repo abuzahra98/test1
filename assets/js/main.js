da9cbffbf6bb0a0a878d9956400aa77d4a40d04e
“dawood-abuzahra” <dabuzahra@pando-soft.com>
2023-10-19 09:25:42 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,15 +23,17 @@ lis.forEach(li =>{
  
 document.addEventListener("DOMContentLoaded", function () {
     var header = document.querySelector("header");
+    // var about1 = document.querySelector("about__section");
     var newContent = document.querySelector(".new-content");
     var image = document.querySelector(".content-about_img");
     var ceo = document.querySelector(".about__section");
-    var aaa = document.querySelector(".image-about_section");
+    var aaa = document.querySelector(".move__text");
     var ccc = document.querySelector(".content-about_section");
   
     window.addEventListener("scroll", function () {
       // Get the height of the header
       var headerHeight = header.offsetHeight;
+      var about1Height = headerHeight + 500;
       var headerwedth = header.offsetWidth;
 
       // Get the current scroll position
@@ -48,10 +50,9 @@ document.addEventListener("DOMContentLoaded", function () {
       if (scrollPosition > headerHeight ) {
          
         ceo.classList.add("about__sectionhover");
-        
     }
-    if (scrollPosition > headerHeight ) { 
-      ceo.classList.add("about__sectionhover");
+    if (scrollPosition > about1Height ) { 
+      aaa.classList.add("move__texthover");
   }
       //  if (headerwedth < 900) {
       //   aaa.style.display = "flex";
